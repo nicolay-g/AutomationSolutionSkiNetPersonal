ef3e98f8a87853b12f707b2894b5b5653a8993c8
nicolay-g <ng82@mail.bg>
2022-08-21 10:56:05 +0300

    Initial DriverManager implementation

--- SkiNet.Library/DriverManager.cs
+++ SkiNet.Library/DriverManager.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,34 @@ using System.Threading.Tasks;
 
 namespace SkiNet.Library
 {
-    class DriverManager
+    public class DriverManager
     {
+        private const string DefaultUrl = "https://skinet.herokuapp.com/shop";
+
+        private static IWebDriver _driver;
+        public IWebDriver Driver => _driver;
+
+        public void Start()
+        {
+            if (_driver == null)
+            {
+                _driver = new ChromeDriver();
+                LoadUrl();
+            }
+        }
+
+        private void LoadUrl(string url = null)
+        {
+            if (url == null)
+            {
+                //url = Config.Configuration.Url;
+                url = DefaultUrl;
+            }
+
+            Driver.Url = url;
+            Driver.Manage().Window.Maximize();
+        }
+
+        public void Quit() => Driver.Quit();
     }
 }
